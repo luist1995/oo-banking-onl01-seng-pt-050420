1995c94ff3aeb7dda1d20baaf8bece7e62223d63
Luis F Tavarez Valdez <luis.tavarez1995@yahoo.com>
2020-06-17 00:41:28 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,36 @@
 class Transfer
   # your code here
+
+  attr_accessor :status
+  attr_reader :sender, :receiver, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    if @status == "pending" && sender.balance > amount && valid?
+      sender.balance-=amount
+      receiver.balance+=amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if execute_transaction
+      sender.balance+=amount
+      receiver.balance-=amount
+      @status = "reversed"
+    end
+  end
 end
